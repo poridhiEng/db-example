c7cb3144503ded40a895ec4161bb7cb50b634bde
umarfchy <umarfchy@gmail.com>
2023-05-20 23:07:33 +0600

    search elasticsearch

--- elasticsearch/elasticsearch.js
+++ elasticsearch/elasticsearch.js
@@ -3,36 +3,36 @@ const { Client } = require("elasticsearch");
 // Create a new Elasticsearch client instance
 const client = new Client({ node: "http://localhost:9200" });
 
-// // Perform a simple search
+// Perform a simple search
 client
   .search({
     index: "kibana_sample_data_ecommerce",
     body: {
       query: {
-        match: { "products.manufacturer": "Elitelligence" },
+        match: { "products.manufacturer": "Low" },
       },
     },
   })
   .then((response) => {
-    console.log(response.hits.hits);
+    console.log(JSON.stringify(response, null, 2));
   })
   .catch((error) => {
     console.error(error);
   });
 
 // Function to list all indices
-async function listIndices() {
-  try {
-    const response = await client.cat.indices({
-      format: "json",
-    });
+// async function listIndices() {
+//   try {
+//     const response = await client.cat.indices({
+//       format: "json",
+//     });
 
-    const indices = await response.map(entry => entry.index);
-    console.log('Indices:', indices);
-  } catch (error) {
-    console.error("Error listing indices:", error);
-  }
-}
+//     const indices = await response.map((entry) => entry.index);
+//     console.log("Indices:", indices);
+//   } catch (error) {
+//     console.error("Error listing indices:", error);
+//   }
+// }
 
 // Call the function to list indices
-listIndices();
+// listIndices();
